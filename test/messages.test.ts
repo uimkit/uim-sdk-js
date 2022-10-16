46ea8ad775d1359b25af28b7d0a2eb1f0f38ad58
fioman <fioman@qq.com>
2022-10-16 17:51:37 +0800

    typo

--- test/messages.test.ts
+++ test/messages.test.ts
@@ -8,13 +8,13 @@ describe("messages", () => {
     const textMessage: TextMessagePayload = {
       type: MessageType.Text,
       body: {
-        content: "你好啊"
-      }
+        content: "你好啊",
+      },
     }
     await client.sendPrivateMessage({
       from: "Sax3MES_sIdvV0sQO9-co@douyin",
       to: "f7942cb5-6177-4351-9eb7-5a958a10b94d@douyin",
-      message: textMessage
+      message: textMessage,
     })
   })
 })
