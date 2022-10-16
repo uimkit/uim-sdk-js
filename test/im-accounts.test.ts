625ae234878e4499824e8f57c1279e45b95b3157
fioman <fioman@qq.com>
2022-10-16 13:31:53 +0800

    test: im-accounts 测试用例

--- test/im-accounts.test.ts
+++ test/im-accounts.test.ts
@@ -3,7 +3,15 @@ import { createClient } from "./client"
 const client = createClient()
 
 describe("im-accounts", () => {
-  it("list im accounts", async () => {
-    await client.imAccounts.list({})
+  it("list-im-accounts", async () => {
+    const resp = await client.imAccounts.list({})
+    expect(resp.data.length).toBeGreaterThan(0)
+    expect(resp.extra.total).toBeGreaterThan(0)
+    // console.log(JSON.stringify(resp, undefined, 4))
+  })
+
+  it("retrieve-im-account", async () => {
+    const resp = await client.imAccounts.retrieve({ account_id: "Sax3MES_sIdvV0sQO9-co" })
+    console.log(JSON.stringify(resp, undefined, 4))
   })
 })
