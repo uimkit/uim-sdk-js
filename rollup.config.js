91500e9b436c5dda30f07519d7befc46c514a01e
fioman <fioman@qq.com>
2023-03-06 19:15:30 +0800

    refactor: 基于 rollup 分别打包 node/browser/browser full

---
+++ rollup.config.js
@@ -0,0 +1,113 @@
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+import pkg from './package.json';
+
+import process from 'process';
+process.env.NODE_ENV = 'production';
+
+const externalPackages = ['axios', 'form-data', 'isomorphic-ws', 'base64-js', /@babel\/runtime/];
+
+const browserIgnore = {
+	name: 'browser-remapper',
+	resolveId: (importee) => (['jsonwebtoken', 'https', 'crypto'].includes(importee) ? importee : null),
+	load: (id) => (['jsonwebtoken', 'https', 'crypto'].includes(id) ? 'export default null;' : null),
+};
+
+const extensions = ['.mjs', '.json', '.node', '.js', '.ts'];
+
+const babelConfig = {
+	babelHelpers: 'runtime',
+	exclude: 'node_modules/**',
+	extensions,
+};
+
+const baseConfig = {
+	input: 'src/index.ts',
+	cache: false,
+	watch: {
+		chokidar: false,
+	},
+};
+const normalBundle = {
+	...baseConfig,
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+			sourcemap: true,
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+			sourcemap: true,
+		},
+	],
+	external: externalPackages.concat(['https', 'jsonwebtoken', 'crypto']),
+	plugins: [
+		replace({ preventAssignment: true, 'process.env.PKG_VERSION': JSON.stringify(pkg.version) }),
+		external(),
+		// TODO formidable 中的代码 if (global.GENTLY) require = GENTLY.hijack(require); 必须要设置 browser: true
+		nodeResolve({ extensions, browser: true }),
+		babel(babelConfig),
+		commonjs(),
+		json(),
+	],
+};
+
+const browserBundle = {
+	...baseConfig,
+	output: [
+		{
+			file: pkg.browser[pkg.main],
+			format: 'cjs',
+			sourcemap: true,
+		},
+		{
+			file: pkg.browser[pkg.module],
+			format: 'es',
+			sourcemap: true,
+		},
+	],
+	external: externalPackages,
+	plugins: [
+		replace({ preventAssignment: true, 'process.env.PKG_VERSION': JSON.stringify(pkg.version) }),
+		browserIgnore,
+		external(),
+		// TODO formidable 中的代码 if (global.GENTLY) require = GENTLY.hijack(require); 必须要设置 browser: true
+		nodeResolve({ extensions, browser: true }),
+		babel(babelConfig),
+		commonjs(),
+		json(),
+	],
+};
+const fullBrowserBundle = {
+	...baseConfig,
+	output: [
+		{
+			file: pkg.jsdelivr,
+			format: 'iife',
+			name: 'window', // write all exported values to window
+			extend: true, // extend window, not overwrite it
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		replace({ preventAssignment: true, 'process.env.PKG_VERSION': JSON.stringify(pkg.version) }),
+		browserIgnore,
+		external(),
+		nodeResolve({ extensions, browser: true }),
+		babel(babelConfig),
+		commonjs(),
+		terser(),
+		json(),
+	],
+};
+
+export default () =>
+	process.env.ROLLUP_WATCH ? [normalBundle, browserBundle] : [normalBundle, browserBundle, fullBrowserBundle];
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,8 @@
 import { UIMClient } from "./client"
-
 export default UIMClient
-export { UIMClientOptions } from "./client"
-
+export * from "./client"
 export * from "./api-endpoints"
-export * from "./models"
 export * from "./events"
-
-export { LogLevel, Logger } from "./logging"
-export {
-  // Error codes
-  UIMErrorCode,
-  APIErrorCode,
-  ClientErrorCode,
-  // Error types
-  UIMClientError,
-  APIResponseError,
-  UnknownHTTPResponseError,
-  RequestTimeoutError,
-  // Error helpers
-  isUIMClientError,
-} from "./errors"
+export * from "./models"
+export * from "./logging"
+export * from "./errors"
