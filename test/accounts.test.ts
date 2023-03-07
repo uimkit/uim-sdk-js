d303959e4e967e8916db5288d12b34b54d317180
fioman <fioman@qq.com>
2023-03-08 03:47:41 +0800

    test: 单元测试 getContact

--- test/accounts.test.ts
+++ test/accounts.test.ts
@@ -13,23 +13,12 @@ describe('accounts', () => {
   });
 
   it('get account', async () => {
-    const account = await client.getAccount('7QWyWEYmpgJQFi_f5nQP2');
-    expect(account.id).toBe('7QWyWEYmpgJQFi_f5nQP2');
+    const account = await client.getAccount('gmtIYyV1ovBPegHPVNOKO');
+    expect(account.id).toBe('gmtIYyV1ovBPegHPVNOKO');
     expect(account.nickname).not.toBe('');
     expect(account.nickname).not.toBeUndefined();
     expect(account.nickname).not.toBeNull();
     expect(account.gender).toBe(Gender.Male);
     console.log(JSON.stringify(account, undefined, 4));
-
-    try {
-      await client.getAccount('abcde');
-    } catch (e) {
-      const apiErr = e as APIResponseError;
-      expect(apiErr.code).toBe(APIErrorCode.Unauthorized);
-      expect(apiErr.message).not.toBe('');
-      expect(apiErr.message).not.toBeUndefined();
-      expect(apiErr.message).not.toBeNull();
-      console.log(`[${apiErr.code}] ${apiErr.message}`);
-    }
   });
 });
--- test/contacts.test.ts
+++ test/contacts.test.ts
@@ -4,7 +4,7 @@ describe('contacts', () => {
   const client = buildClient();
 
   it("list account's contacts", async () => {
-    const resp1 = await client.listContacts({ account_id: '7QWyWEYmpgJQFi_f5nQP2', limit: 5 });
+    const resp1 = await client.listContacts({ account_id: 'gmtIYyV1ovBPegHPVNOKO', limit: 5 });
     expect(resp1.data.length).toBe(5);
     expect(resp1.extra.start_cursor).not.toBe('');
     expect(resp1.extra.end_cursor).not.toBe('');
@@ -13,7 +13,7 @@ describe('contacts', () => {
     console.log(JSON.stringify(resp1, undefined, 4));
 
     const resp2 = await client.listContacts({
-      account_id: '7QWyWEYmpgJQFi_f5nQP2',
+      account_id: 'gmtIYyV1ovBPegHPVNOKO',
       limit: 5,
       cursor: resp1.extra.end_cursor,
     });
@@ -24,10 +24,17 @@ describe('contacts', () => {
     expect(resp2.extra.has_previous).toBe(true);
     console.log(JSON.stringify(resp2, undefined, 4));
 
-    const resp3 = await client.listContacts({ account_id: '7QWyWEYmpgJQFi_f5nQP2', limit: 10 });
+    const resp3 = await client.listContacts({ account_id: 'gmtIYyV1ovBPegHPVNOKO', limit: 10 });
     expect(resp3.data[0].id).toBe(resp1.data[0].id);
     expect(resp3.data[4].id).toBe(resp1.data[4].id);
     expect(resp3.data[5].id).toBe(resp2.data[0].id);
     expect(resp3.data[9].id).toBe(resp2.data[4].id);
+  });
+
+  it('get contact', async () => {
+    const contact = await client.getContact('c_7ph1oQe9KPDTaA8Ke4d');
+    expect(contact.id).toBe('c_7ph1oQe9KPDTaA8Ke4d');
+    expect(contact.account).toBe('gmtIYyV1ovBPegHPVNOKO');
+    console.log(JSON.stringify(contact, undefined, 4));
   });
 });
