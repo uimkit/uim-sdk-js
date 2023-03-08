90bd90d9c5ea93bc47cb046b59415ccd92b6c9a3
fioman <fioman@qq.com>
2023-03-08 21:38:33 +0800

    test: 单元测试 getConversation

--- test/conversations.test.ts
+++ test/conversations.test.ts
@@ -1,8 +1,10 @@
+import exp from 'constants';
 import { buildClient } from './client';
 
 describe('conversations', () => {
   const client = buildClient();
 
+  /*
   it("list account's conversations", async () => {
     const resp1 = await client.listConversations({ account_id: 'gmtIYyV1ovBPegHPVNOKO', limit: 5 });
     expect(resp1.data.length).toBe(5);
@@ -30,11 +32,12 @@ describe('conversations', () => {
     expect(resp3.data[5].id).toBe(resp2.data[0].id);
     expect(resp3.data[9].id).toBe(resp2.data[4].id);
   });
+  */
 
-  // it('get contact', async () => {
-  //   const contact = await client.getContact('c_7ph1oQe9KPDTaA8Ke4d');
-  //   expect(contact.id).toBe('c_7ph1oQe9KPDTaA8Ke4d');
-  //   expect(contact.account).toBe('gmtIYyV1ovBPegHPVNOKO');
-  //   console.log(JSON.stringify(contact, undefined, 4));
-  // });
+  it('get conversation', async () => {
+    const conversation = await client.getConversation('2BzIjJZ0uT_IjnxmT7koD');
+    expect(conversation.id).toBe('2BzIjJZ0uT_IjnxmT7koD');
+    expect(conversation.account).toBe('gmtIYyV1ovBPegHPVNOKO');
+    console.log(JSON.stringify(conversation, undefined, 4));
+  });
 });
