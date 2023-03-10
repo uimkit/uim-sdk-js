059bfb4cf0540b01a97b2fa6f98b019c037682cb
fioman <fioman@qq.com>
2023-03-10 15:19:18 +0800

    test: 单元测试 deleteMessage

--- test/messages.test.ts
+++ test/messages.test.ts
@@ -3,19 +3,21 @@ import { buildClient } from './client';
 describe('messages', () => {
   const client = buildClient();
 
-  it('list conversation messages', async () => {
-    const resp = await client.listMessages({ conversation_id: '2BzIjJZ0uT_IjnxmT7koD', limit: 5 });
-    expect(resp.data.length).toBeGreaterThan(0);
-    console.log(JSON.stringify(resp, undefined, 4));
+  // it('list conversation messages', async () => {
+  //   const resp = await client.listMessages({ conversation_id: '2BzIjJZ0uT_IjnxmT7koD', limit: 5 });
+  //   expect(resp.data.length).toBeGreaterThan(0);
+  //   console.log(JSON.stringify(resp, undefined, 4));
+  // });
+
+  it('delete message', async () => {
+    const conversation1 = await client.getConversation('2BzIjJZ0uT_IjnxmT7koD')
+    const lastMessageId = conversation1.last_message?.id
+    expect(lastMessageId).not.toBe('')
+    await client.deleteMessage(lastMessageId!)
+    const conversation2= await client.getConversation('2BzIjJZ0uT_IjnxmT7koD')
+    expect(conversation2.last_message).not.toBeUndefined()
+    expect(conversation2.last_message).not.toBeNull()
+    expect(conversation2.last_message?.id).not.toBe(lastMessageId)
   });
 
-  // it('get account', async () => {
-  //   const account = await client.getAccount('gmtIYyV1ovBPegHPVNOKO');
-  //   expect(account.id).toBe('gmtIYyV1ovBPegHPVNOKO');
-  //   expect(account.nickname).not.toBe('');
-  //   expect(account.nickname).not.toBeUndefined();
-  //   expect(account.nickname).not.toBeNull();
-  //   expect(account.gender).toBe(Gender.Male);
-  //   console.log(JSON.stringify(account, undefined, 4));
-  // });
 });
